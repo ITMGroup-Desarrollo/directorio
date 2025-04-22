4cae1819f7fad6c8d25c1b0026297c83826906cc
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-04-22 12:07:47 -0500

    feat: add image support to vCard generation API and update contact sharing link

--- src/pages/api/contacto.vcf.ts
+++ src/pages/api/contacto.vcf.ts
@@ -1,4 +1,5 @@
 import type { APIRoute } from "astro";
+import { Buffer } from "buffer";
 
 export const GET: APIRoute = async ({ url }) => {
   const nombre = url.searchParams.get("nombre") || "Contacto";
@@ -6,6 +7,20 @@ export const GET: APIRoute = async ({ url }) => {
   const correo = url.searchParams.get("correo") || "";
   const celular = url.searchParams.get("celular") || "";
   const puesto = url.searchParams.get("puesto") || "";
+  const imagenUrl = url.searchParams.get("imagen") || "";
+
+  let photoBase64 = "";
+
+  if (imagenUrl) {
+    try {
+      const res = await fetch(imagenUrl);
+      const arrayBuffer = await res.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      photoBase64 = buffer.toString("base64");
+    } catch (err) {
+      console.error("Error al convertir la imagen:", err);
+    }
+  }
 
   const vcard = `BEGIN:VCARD
 VERSION:3.0
@@ -14,6 +29,7 @@ FN:${nombre} ${apellido}
 TITLE:${puesto}
 TEL;TYPE=CELL:${celular}
 EMAIL:${correo}
+${photoBase64 ? `PHOTO;ENCODING=b;TYPE=JPEG:${photoBase64}` : ""}
 END:VCARD
 `;
 
