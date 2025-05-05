8ca4e35ea859533b21b23759f656132822a31e4c
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-05-05 12:17:48 -0500

    fix: update asset paths to include new directory for image loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/directorio/',
+  base: '/directorioitm/',
   vite: {
     plugins: [tailwindcss()]
   }
--- src/data/personas.js
+++ src/data/personas.js
@@ -6,7 +6,7 @@ export const data = [
       puesto: "Director de Tecnología",
       correo: "juan.perez@example.com",
       celular: "+529983714396",
-      imagen: "/directorio/assets/images/paulo.png"
+      imagen: "/directorioitm/assets/images/paulo.png"
     },
     {
       id: "2",
@@ -15,7 +15,7 @@ export const data = [
       puesto: "Gerente de Marketing",
       correo: "laura.garcia@example.com",
       celular: "+529983714396",
-      imagen: "/directorio/assets/images/laura-garcia.jpg"
+      imagen: "/directorioitm/assets/images/laura-garcia.jpg"
     }
   ];
   
