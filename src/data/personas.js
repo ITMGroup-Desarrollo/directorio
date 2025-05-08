30b88c5f61207a1839be4fe76ab1e2beab54a3f9
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-05-08 09:03:21 -0500

    fix: update image paths in persona data to include '/directorio/' for correct resource loading

--- src/data/personas.js
+++ src/data/personas.js
@@ -6,7 +6,7 @@ export const data = [
       puesto: "Gerente de Ventas",
       correo: "pgaliano@itmgroup.mx",
       celular: "+529981686808",
-      imagen: "/assets/images/paulo.png"
+      imagen: "/directorio/assets/images/paulo.png"
     },
     {
       id: "2",
@@ -15,7 +15,7 @@ export const data = [
       puesto: "Gerente de Marketing",
       correo: "laura.garcia@example.com",
       celular: "+529983714396",
-      imagen: "/assets/images/laura-garcia.jpg"
+      imagen: "/directorio/assets/images/laura-garcia.jpg"
     }
   ];
   
